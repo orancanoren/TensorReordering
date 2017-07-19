d71f646f988a8593bc412d47c33d4ed6ebac3c55
Oran Can Ören <orancanoren@gmail.com>
2017-07-19 17:54:31 +0300

    filename is now a command line input

--- PURE/main.cpp
+++ PURE/main.cpp
@@ -5,8 +5,16 @@
 
 using namespace std;
 
-int main() {
-	const string filename = "sample_graph.txt";
+int main(int argc, char * argv[]) {
+	string filename;
+	if (argc > 1) {
+		filename = argv[1];
+	}
+	else {
+		cout << "Please enter the filename as a CL argument" << endl;
+		exit(1);
+	}
+
 	ifstream is(filename);
 	if (!is.is_open()) {
 		cout << "Unable to open file " << filename << endl
